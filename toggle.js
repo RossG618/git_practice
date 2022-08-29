d5b7137ac733a114f2ef1e05eb97b94155480417
Ross <iamlightning@hotmail.co.uk>
2022-08-29 17:01:28 +0100

    js func review panel done

--- toggle.js
+++ toggle.js
@@ -1,5 +1,14 @@
+
+
 var toggle = document.getElementById("rev-pop");
-var popup = document.getElementById("review-toggle")
+var popup = document.getElementById("tab-review")
 function togglePop() {
         this.toggle.classList.toggle("close-pop");
+        this.popup.classList.remove("review-tab-remove");
     }    
+
+function unTogglePop(){
+    this.toggle.classList.toggle("close-pop");
+    this.popup.classList.add("review-tab-remove");
+
+}
