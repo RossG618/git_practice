e1e0858478bfaa38edc5e01c8691608b46c78e88
Ross <iamlightning@hotmail.co.uk>
2022-08-29 15:14:09 +0100

    reviews js func part1

---
+++ toggle.js
@@ -0,0 +1,5 @@
+var toggle = document.getElementById("rev-pop");
+var popup = document.getElementById("review-toggle")
+function togglePop() {
+        this.toggle.classList.toggle("close-pop");
+    }    
